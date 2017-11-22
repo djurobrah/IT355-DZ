9a303583be48840b4f0c03f6bb6841c55bcfab8b
Djurica Djuricic <p3ntaphr3ak@gmail.com>
2017-11-22 04:27:30 +0100

    I18N enabled , Local Stylesheet,added
    I18N requires changes in the property file to work. Did that.
    Thymeleaf reference to a stylesheet added.

--- src/main/java/djuricadjuricic/it355dz/It355dzApplication.java
+++ src/main/java/djuricadjuricic/it355dz/It355dzApplication.java
@@ -3,6 +3,7 @@ package djuricadjuricic.it355dz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class It355dzApplication
 {
